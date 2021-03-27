2c968b68a88a7b53f821452b2abe5d5649779ec2
jsungl <wotjd931026@gmail.com>
2021-03-28 00:47:50 +0900

    web_client workspace 업로드

---
+++ 03_javascript/js/sample.js
@@ -0,0 +1,7 @@
+//자바스크립트 공간(스크립트 태그안)
+
+alert('나는 sample.js에서 왔어');
+
+function bar() {
+    alert('sample.js의 bar 실행');
+}
---
+++ 03_javascript/js/sample2.js
@@ -0,0 +1,4 @@
+
+function bar2() {
+    alert('sample2.js의 bar2 실행');
+}
